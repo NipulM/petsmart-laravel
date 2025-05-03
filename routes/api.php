c98ea3580211d36aed608cb6614e8fc03f6b8eec
Nipul Mallikarachchi <nipulm03@gmail.com>
2025-05-03 22:23:24 +0530

    impr: updated setup

--- routes/api.php
+++ routes/api.php
@@ -3,26 +3,33 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\Auth\ApiAuthenticatedSessionController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\AuthController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
 
-Route::prefix('categories')->group(function () {
-    Route::get('/', [CategoryController::class, 'getAllCategories']);
-    Route::get('/{id}', [CategoryController::class, 'getCategoryById']);
-    // Route::post('/', [CategoryController::class, 'createNewCategory']);
-});
+    // Categories routes
+    Route::prefix('categories')->group(function () {
+        Route::get('/', [CategoryController::class, 'getAllCategories']);
+        Route::get('/{id}', [CategoryController::class, 'getCategoryById']);
+        Route::post('/', [CategoryController::class, 'createNewCategory']);
+    });
 
-Route::prefix('products')->group(function () {
-    Route::get('/', [ProductController::class, 'getAllProducts']);
-    Route::get('/new', [ProductController::class, 'getNewProducts']);
-    Route::get('/filter', [ProductController::class, 'filterProducts']);
-    Route::get('/{id}', [ProductController::class, 'getProductById']);
-    // Route::post('/', [ProductController::class, 'createProduct']);
-    // Route::put('/{id}', [ProductController::class, 'updateProduct']);
-    // Route::delete('/{id}', [ProductController::class, 'deleteProduct']);
+    // Products routes
+    Route::prefix('products')->group(function () {
+        Route::get('/', [ProductController::class, 'getAllProducts']);
+        Route::get('/new', [ProductController::class, 'getNewProducts']);
+        Route::get('/filter', [ProductController::class, 'filterProducts']);
+        Route::get('/{id}', [ProductController::class, 'getProductById']);
+        Route::post('/', [ProductController::class, 'createProduct']);
+        Route::put('/{id}', [ProductController::class, 'updateProduct']);
+        Route::delete('/{id}', [ProductController::class, 'deleteProduct']);
+    });
 });
